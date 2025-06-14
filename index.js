b613eea6de6bfddc69047a1e06d160e594dbf130
TechyShie <susanshie316@gmail.com>
2025-06-14 11:47:41 +0300

    Complete scope lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() { // spelling must match!
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'SHie';
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Ralph'; // this will fail, and that's expected
+}
+
+
